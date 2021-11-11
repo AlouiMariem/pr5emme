521127fb12523a8a8f70c011a08a2466cf5682cc
imen <imen.amami36@gmail.com>
2021-11-11 19:17:22 +0100

    routage  model vente

--- frontend/src/app/vente/vente-routing.module.ts
+++ frontend/src/app/vente/vente-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ArticleDetailsComponent } from './article-details/article-details.component';
+import { ArticleListComponent } from './article-list/article-list.component';
+import { NewArticleComponent } from './new-article/new-article.component';
+import { UpdateArticleComponent } from './update-article/update-article.component';
 
-const routes: Routes = [];
+const routes: Routes = [  {path: 'articles', component: ArticleListComponent},
+{path: 'create-article', component: NewArticleComponent},
+{path: '', redirectTo: 'articles', pathMatch: 'full'},
+{path: 'update-article/:id', component: UpdateArticleComponent},
+{path: 'article-details/:id', component: ArticleDetailsComponent}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
