fa465e15e9af7ac57a5b637c4b85b48a01eea540
AlouiMariem <88490696+AlouiMariem@users.noreply.github.com>
2021-11-11 22:30:08 +0100

    Revert "buy"

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,18 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ShoppingCardComponent } from './admin/shopping-card/shopping-card.component';
-import { RechercheComponent } from './admin/recherche/recherche.component';
-import { ReminderComponent } from './admin/reminder/reminder.component';
-import { BuyArticleComponent } from './admin/buy-article/buy-article.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ShoppingCardComponent,
-    RechercheComponent,
-    ReminderComponent,
-    BuyArticleComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
