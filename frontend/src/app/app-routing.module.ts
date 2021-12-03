786058a9f2934aa541ee47d2063a306d17fddf9f
AlouiMariem <88490696+AlouiMariem@users.noreply.github.com>
2021-12-03 05:12:41 +0100

    t-login1

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {CreateComptesComponent} from "./gestion-des-comptes/client/create-comptes/create-comptes.component"
-
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 const routes: Routes = [
   { path: 'path', loadChildren: () => import('./gestion-des-comptes/gestion-des-comptes.module').then(m => m.GestionDesComptesModule) },
   { path: 'path', loadChildren: () => import('./buy/buy.module').then(m => m.BuyModule) }, 
   { path: 'path', loadChildren: () => import('./buy/buy.module').then(m => m.BuyModule) },
-  { path: 'signUp', component: CreateComptesComponent }
+  { path: 'signUp', component: CreateComptesComponent },
+  { path: 'Home', component: HomeComponent},
+  { path: 'login', component: LoginComponent}
 ];
 
 
